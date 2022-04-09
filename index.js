f5dc0ad130ce0ec5fa4067ae12c4f446457fdd1c
Azadeh <ghobadi.azade@gmail.comconfig --global azadeh ghobadi>
2022-04-09 00:06:16 -0400

    REST-API-MongoDb

--- index.js
+++ index.js
@@ -6,19 +6,24 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const userRouts = require("./routes/users");
 const authRouts = require("./routes/auth");
+const postRoute = require("./routes/posts");
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () => {
   console.log("Connect to MongoDB");
 });
-//middleware jhdhwuk
-//test
-//changes
+
+//middleware
+
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
+
 app.use("/api/users", userRouts);
 app.use("/api/auth", authRouts);
+app.use("/api/posts", postRoute);
+
 app.listen(8000, () => {
   console.log("Backend Server is running");
 });
