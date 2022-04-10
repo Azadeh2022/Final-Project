73a4643e79bd27411fbf3aedf5bf43ad177c69e7
Azadeh <ghobadi.azade@gmail.comconfig --global azadeh ghobadi>
2022-04-09 23:13:17 -0400

    commit-test

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,5 +1,9 @@
 import React from "react";
+import Topbar from "../../components/topbar/Topbar";
 
 export default function Home() {
-  return <div>this is Home</div>;
+  return (
+<Topbar/>
+  )
+
 }
