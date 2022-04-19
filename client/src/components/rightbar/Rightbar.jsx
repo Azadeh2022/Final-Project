68134275cea5e0a4f1e5e73e981a5f43d54a4b0c
Azadeh <ghobadi.azade@gmail.comconfig --global azadeh ghobadi>
2022-04-19 01:14:07 -0400

    commit

--- client/src/components/rightbar/Rightbar.jsx
+++ client/src/components/rightbar/Rightbar.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import "./rightbar.css"
 
 export default function Rightbar() {
   return (
-    <div>Rightbar</div>
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <div className="logoContainer">
+          <img className="logoImg" src="assets/logo.png" alt="" />
+          <span className="logoText">
+            <b>Historical Places </b> Photo and Video
+          </span>
+        </div>
+        <img className="rightbarAd" src="assets/ad.png" alt="" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+      </div>
+    </div>
   )
 }
