99cfcf76f7a48f1f778ecf2d0424983c0bdf4700
Azadeh <ghobadi.azade@gmail.comconfig --global azadeh ghobadi>
2022-04-18 23:47:00 -0400

    commit

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -1,7 +1,33 @@
-import "./post.css"
+import "./post.css";
+import { MoreVert } from "@material-ui/icons";
 
-export default function Post() {
+
+export default function Post({ post }) {
+  
   return (
-    <div>Hello post</div>
-  )
+    <div className="post">
+      <div className="postWrapper">
+        <div className="postTop">
+          <div className="postTopLeft">
+           
+          
+          </div>
+          <div className="postTopRight">
+            <MoreVert />
+          </div>
+        </div>
+        <div className="postCenter">
+        
+        </div>
+        <div className="postBottom">
+          <div className="postBottomLeft">
+           
+          </div>
+          <div className="postBottomRight">
+           
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
