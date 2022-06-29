fa0673058ff55ba39076dbbbcfecaca7584eb397
Azadeh <ghobadi.azade@gmail.comconfig --global azadeh ghobadi>
2022-06-28 21:50:56 -0400

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,20 @@
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./pages/register/Register";
+import { BrowserRouter, Routes, Route,  Redirect } from "react-router-dom";
+
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
 function App() {
-  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  const { user } = useContext(AuthContext);
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<Home />} />
+        <Route exact path="/" element= {user ? <Home /> : <Register />} />
 
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
 
         <Route path="/profile/:username" element={<Profile />} />
       </Routes>
